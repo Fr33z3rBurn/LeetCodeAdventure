9a52cc292c44ac6d6f589b1598c1286aab1e925e
Tim Freese <freese.tim@gmail.com>
2022-10-11 23:32:44 -0500

    October

---
+++ LeetCodeAdventure/October2022/LargestPerimeterTriangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCodeAdventure.October2022
+{
+    public class LargestPerimeterTriangle
+    {
+        public int LargestPerimeter(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = nums.Count() - 1; i >= 2; i--)
+            {
+                if (nums[i] < nums[i - 1] + nums[i - 2]) return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+            return 0;
+        }
+    }
+}
+
