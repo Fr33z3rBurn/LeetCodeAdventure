49ff716ef7a878397c17d7f2ccea470c08023955
Tim Freese <freese.tim@gmail.com>
2022-10-19 22:22:28 -0500

    oct 18

---
+++ LeetCodeAdventure/October2022/TopKFrequentWords.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCodeAdventure.October2022
+{
+	public class TopKFrequentWords
+	{
+		public IList<string> TopKFrequent(string[] words, int k)
+		{
+			var distinctWords = words.ToArray().Select(x => x).Distinct().ToArray();
+			var tupleList = new List<Tuple<int, string>>();
+			foreach (var word in distinctWords)
+			{
+				var count = words.Where(x => x == word).Count();
+				var theTuple = Tuple.Create<int, string>(count, word);
+				tupleList.Add(theTuple);
+			}
+			var sortArray = tupleList.ToArray();
+			Array.Sort(sortArray);
+			Array.Reverse(sortArray);
+
+			var anotherList = sortArray.OrderByDescending(x => x.Item1).ThenBy(y => y.Item2).ToList();
+			var returnList = new List<string>();
+			for (int i = 0; i < k; i++)
+			{
+				returnList.Add(anotherList[i].Item2);
+			}
+
+			return returnList;
+		}
+	}
+}
+
