87de01045bc7bbea6b83d4f986f2f9f388bf7c1e
Tim Freese <freese.tim@gmail.com>
2022-10-22 23:24:33 -0500

    Oct 22

---
+++ LeetCodeAdventure/October2022/SetMismatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCodeAdventure.October2022
+{
+	//https://leetcode.com/problems/set-mismatch/
+	//TODO
+	public class SetMismatch
+	{
+		public int[] FindErrorNums(int[] nums)
+		{
+			var dupNum = nums.GroupBy(x => x).Where(z => z.Count() == 2).Select(y => y.Key).FirstOrDefault();
+			var edge = new int[] { 2, 2 };
+			if (dupNum == 2 && (nums.Contains(3) || nums.Length == 2))
+			{
+				return new int[] { 2, 1 };
+			}
+			return new int[] { dupNum, dupNum + 1 };
+		}
+	}
+}
+
