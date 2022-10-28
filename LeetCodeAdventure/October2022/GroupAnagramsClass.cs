58d221223fa465e474415389227d53033d57b391
Tim Freese <freese.tim@gmail.com>
2022-10-27 23:02:39 -0500

    Oct 27

---
+++ LeetCodeAdventure/October2022/GroupAnagramsClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LeetCodeAdventure.October2022
+{
+	//https://leetcode.com/problems/group-anagrams/description/
+	//TODO
+	public class GroupAnagramsClass
+	{
+		public IList<IList<string>> GroupAnagrams(string[] strs)
+		{
+
+		}
+	}
+}
+
