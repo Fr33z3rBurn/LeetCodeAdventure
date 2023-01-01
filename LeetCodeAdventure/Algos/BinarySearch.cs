8f5a9846abf29181e687f2fa8d95b7e8c33fbaed
Tim Freese <freese.tim@gmail.com>
2022-12-31 22:23:48 -0600

    binary search

---
+++ LeetCodeAdventure/Algos/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAdventure.Algos
+{
+	public static class BinarySearch
+	{
+		public static int PerformBinarySearch(int[] array, int key, int left, int right)
+		{
+			if (left > right)
+			{
+				return -1;
+			}
+			else
+			{
+				int mid = (left - right) / 2;
+				if(key == array[mid])
+				{
+					return mid;
+				}
+				else if (key < array[mid])
+				{
+					return PerformBinarySearch(array, key, left, mid - 1);
+				}
+				else if ( key > array[mid])
+				{
+					return PerformBinarySearch(array, key, mid + 1, right);
+				}
+				return -1;
+			}
+		}
+	}
+}
